5173a16c6487880aa50e52a8eccff3c68d8463f1
Developer <Developer@DESKTOP-F77GV6B>
2017-02-13 20:24:19 -0600

    Shooter is closer to final version

--- src/Shooter.cpp
+++ src/Shooter.cpp
@@ -1,9 +1,5 @@
-/*
- * Shooter.cpp
- *
- *  Created on: Jan 14, 2017
- *      Author: Developer
- */
+// Shooter.cpp
+
 
 #include <Shooter.h>
 #include <const.h>
@@ -16,74 +12,118 @@
 
 Shooter::Shooter(OperatorInputs *operatorinputs)
 {
-	// TODO Auto-generated constructor stub
-	m_shootermotor = new CANTalon (CAN_SHOOTER_MOTOR);
 	m_inputs = operatorinputs;
-	m_feedmotor = new Talon(1);
+	m_shootermotor = new CANTalon(CAN_SHOOTER_MOTOR);
+	m_feedmotor = new CANTalon(CAN_FEED_MOTOR);
+	m_lowrpm = SHOOTER_LOW_RPM;
+	m_shootrpm = SHOOTER_SHOOT_RPM;
+	m_shoot = false;
 }
+
 
 Shooter::~Shooter()
 {
-	// TODO Auto-generated destructor stub
 	delete m_shootermotor;
 	delete m_feedmotor;
-
 }
+
 
 void Shooter::Init()
 {
 	m_shootermotor->ConfigNeutralMode(CANSpeedController::NeutralMode::kNeutralMode_Coast);
 	m_shootermotor->SetFeedbackDevice(CANTalon::FeedbackDevice::QuadEncoder);
 	m_shootermotor->SetControlMode(CANSpeedController::ControlMode::kSpeed);
-	//m_shootermotor->SetControlMode(CANSpeedController::ControlMode::kPercentVbus);
 	m_shootermotor->SetPID(CAN_SHOOTER_P, CAN_SHOOTER_I, CAN_SHOOTER_D, CAN_SHOOTER_F);
 	m_shootermotor->ConfigEncoderCodesPerRev(CAN_SHOOTER_ENCODER_TICKS);
 	m_shootermotor->ClearIaccum();
-	m_shootermotor->EnableControl();
 	m_shootermotor->Enable();
-	SmartDashboard::PutNumber("DB/Slider 0", SD_SHOOTER_SLIDER_DEFAULT);
+	m_shootermotor->Set(0);
+
+	m_feedmotor->SetControlMode(CANSpeedController::ControlMode::kVoltage);
 	m_feedmotor->Set(0);
 
+	m_lowrpm = SHOOTER_LOW_RPM;
+	m_shootrpm = SHOOTER_SHOOT_RPM;
+	m_shoot = false;
 }
+
 
 void Shooter::Stop()
 {
+	m_feedmotor->Set(0);
 	m_shootermotor->Disable();
-	//m_shootermotor->DisableControl();
 }
+
 
 void Shooter::Loop()
 {
-	bool shootertogglebutton = m_inputs->joystickTrigger(OperatorInputs::ToggleChoice::kHold);
+
 	//SmartDashboard::PutString("DB/String 1", "Shooter On: " + std::to_string(!shootertogglebutton));
-	if (!shootertogglebutton == 1)
+
+	/*
+	double slidervalue = SmartDashboard::GetNumber("DB/Slider 0", SD_SHOOTER_SLIDER_DEFAULT);
+	double newrpm = (slidervalue - SD_SHOOTER_SLIDER_DEFAULT) * SHOOTER_SLIDER_TO_RPM;
+	m_shootermotor->Set(newrpm);
+
+	double encodervalue = m_shootermotor->GetSpeed();
+	double velocity = encodervalue * 2 * 3.14159265359 * SHOOTER_RADIUS * (1/60.0); //60 secs per minute
+	double outputvoltage = m_shootermotor->GetOutputVoltage();
+
+ 	 if (!shootertogglebutton)
 	{
 		SmartDashboard::PutString("DB/String 1", "Shooter On: Yes");
 	}
-		else
-		{
-			SmartDashboard::PutString("DB/String 1", "Shooter On: No");
-		}
-
-	double slidervalue = SmartDashboard::GetNumber("DB/Slider 0", SD_SHOOTER_SLIDER_DEFAULT);
-	//double newrpm = (slidervalue - SD_SHOOTER_SLIDER_DEFAULT) * (1.0 / 2.5);
-	double newrpm = (slidervalue - SD_SHOOTER_SLIDER_DEFAULT) * SHOOTER_SLIDER_TO_RPM;
-	//m_feedmotor->Set(newrpm);
-	m_shootermotor->Set(newrpm);
-	SmartDashboard::PutString("DB/String 0", "Input RPM: " + std::to_string(abs(newrpm)));
-	double encodervalue = m_shootermotor->GetSpeed();
-	SmartDashboard::PutString("DB/String 5", "Output RPM: " /*+ std::to_string(encodervalue)*/);
-	SmartDashboard::PutString("DB/String 6", std::to_string(abs(encodervalue)));
-	double velocity = encodervalue * 2 * 3.14159265359 * SHOOTER_RADIUS * (1/60.0); //60 secs per minute
-	SmartDashboard::PutString("DB/String 4", "Velocity: " + std::to_string(abs(velocity)));
-	double outputvoltage = m_shootermotor->GetOutputVoltage();
-	SmartDashboard::PutString("DB/String 2", "Output Voltage: "); //+ std::to_string(abs(outputvoltage)));
-	SmartDashboard::PutString("DB/String 3", std::to_string(abs(outputvoltage)));
-	SmartDashboard::PutString("DB/String 7", "                    Do                    ");
-	SmartDashboard::PutString("DB/String 8", "               Or Do Not                ");
-	SmartDashboard::PutString("DB/String 9", "           There is No Try             ");
-	if (shootertogglebutton == 1)
+	else
 	{
-		m_shootermotor->Set(0);
+		SmartDashboard::PutString("DB/String 1", "Shooter On: No");
+	}
+	SmartDashboard::PutString("DB/String 0", "Input RPM: " + std::to_string(abs(newrpm)));
+	SmartDashboard::PutString("DB/String 5", "Output RPM: " 	//+ std::to_string(encodervalue));
+	SmartDashboard::PutString("DB/String 6", std::to_string(abs(encodervalue)));
+	SmartDashboard::PutString("DB/String 4", "Velocity: " + std::to_string(abs(velocity)));
+	SmartDashboard::PutString("DB/String 2", "Output Voltage: "); 	//+ std::to_string(abs(outputvoltage)));
+	SmartDashboard::PutString("DB/String 3", std::to_string(abs(outputvoltage)));
+	*/
+
+	double speed = m_shootermotor->GetSpeed() * SHOOTER_DIRECTION;
+	bool shooterbutton = m_inputs->xBoxRightBumper();
+	bool rpmup = m_inputs->xBoxDPadUp(OperatorInputs::ToggleChoice::kHold);
+	bool rpmdown = m_inputs->xBoxDPadDown(OperatorInputs::ToggleChoice::kHold);
+
+	if (shooterbutton)
+	{
+		m_shoot = !m_shoot;
+		if (m_shoot)
+			m_shootermotor->Set(m_shootrpm * SHOOTER_DIRECTION);
+		else
+			m_shootermotor->Set(m_lowrpm * SHOOTER_DIRECTION);
+	}
+	if (rpmup)
+	{
+		if (m_shoot)
+			m_shootrpm++;
+		else
+			m_lowrpm++;
+	}
+	if (rpmdown)
+	{
+		if (m_shoot)
+			m_shootrpm--;
+		else
+			m_lowrpm--;
+	}
+	if (m_shoot)
+	{
+		if (abs(speed - m_shootrpm) < (SHOOTER_ERROR_RPM * m_shootrpm))
+		{
+			m_feedmotor->Set(4.0);
+		}
+	}
+	else
+	{
+		m_feedmotor->Set(0);
 	}
 }
+
+
+
--- src/Shooter.h
+++ src/Shooter.h
@@ -1,21 +1,18 @@
-/*
- * Shooter.h
- *
- *  Created on: Jan 14, 2017
- *      Author: Developer
- */
+// Shooter.h
 
 
 #ifndef SRC_SHOOTER_H_
 #define SRC_SHOOTER_H_
 
+
 #include "OperatorInputs.h"
 #include <CANTalon.h>
-#include <Talon.h>
+
 
 using namespace frc;
 
-class Shooter {
+class Shooter
+{
 public:
 	Shooter(OperatorInputs *operatorinputs);
 	~Shooter();
@@ -25,8 +22,10 @@ public:
 protected:
 	OperatorInputs *m_inputs;
 	CANTalon *m_shootermotor;
-	Talon *m_feedmotor;
-
+	CANTalon *m_feedmotor;
+	double m_lowrpm;
+	double m_shootrpm;
+	bool m_shoot;
 };
 
 #endif /* SRC_SHOOTER_H_ */
